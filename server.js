9bd1ef1960a165e6d8b339c89ad4581ff6acab8a
Tanner Read <tread06@gmail.com>
2021-08-21 16:02:21 -0400

    Added request log, index, and documentation endpoints
    Added a log file. Updated server requests so that they all append the request URL to the log. Added endpoints for the documentation and index files.

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response) => {
+
+//assign default path data to index.html
+let filePath = __dirname + '/index.html';
+
+//parse the request
+const baseUrl = request.protocol + '://' + request.headers.host + '/';
+const reqUrl = new URL(request.url, baseUrl);    
+
+//log the request (ignore favicon requests)
+if(reqUrl.pathname != '/favicon.ico'){
+    fs.appendFile('log.txt', 'URL: ' + reqUrl.href +'\nTimestamp: ' + new Date() + '\n\n', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log( reqUrl.href +' logged.');
+        }
+    });
+}    
+
+//assign documentation file path if requested
+if(reqUrl.searchParams.has('documentation')){
+    console.log("documentation request recieved");
+    filePath = __dirname + '/documentation.html';
+}
+
+//return path data
+fs.readFile(filePath, (err, data) => {
+    if (err) {
+        throw err;
+    }
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+});
+
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
