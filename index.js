7da9f7c02041d86ef676c3c578f4100cd793279b
Tanner Read <tread06@gmail.com>
2021-09-07 08:22:08 -0400

    Added local host development domain restriction
    Added CORS middleware with only local host as the allowed domain.

--- index.js
+++ index.js
@@ -23,6 +23,20 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+//cors
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080'];
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+        return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 //auth (must be placed after body parsing)
 let auth = require('./auth')(app); 
 const passport = require('passport');
