c90b80df51c8bd5b3ef3bff2868b34c7c8e945c3
Tanner Read <tread06@gmail.com>
2021-09-08 07:43:44 -0400

    Changed users endpoints to not return passwords
    Updated user endpoints to omit the password field from the user data.

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.get('/movies/:title', passport.authenticate('jwt', { session: false }), (req
     Birthday: Date      Birthday
 }*/
 app.post('/users', [  
-    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username is required').isLength({min: 3}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
@@ -127,9 +127,9 @@ app.post('/users', [
 
 // Get all users
 app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.find()
+    Users.find({}, 'Username Email Birthday FavoriteMovies')
     .then((users) => {
-            res.status(201).json(users);
+            res.status(201).json({users});
     })
     .catch((err) => {
         console.error(err);
@@ -139,7 +139,7 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
 
 // Get a user by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.findOne({ Username: req.params.Username })
+    Users.findOne({ Username: req.params.Username }, 'Username Email Birthday FavoriteMovies')
     .then((user) => {
             res.json(user);
     })
