c96f912e08ca8ac3ab22a7db1eecd5bc6a0ccf01
Tanner Read <tread06@gmail.com>
2021-09-09 04:27:18 -0400

    Setup Heroku
    Added start script to package.js. Updated app.listen() to use environment variable for the port.

--- index.js
+++ index.js
@@ -279,4 +279,8 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('An error occured.');
 });
-app.listen(8080);
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+    console.log('Listening on Port ' + port);
+});
