61c7abcafe8801337b2e332141c5593e5dc33ada
Tanner Read <tread06@gmail.com>
2021-09-15 15:09:53 -0400

    Temporarily removed authentication from the movies endpoints for testing.
    Temporarily removed the passport middleware from the movie endpoints in order to rest react components in the CineFacts  frontend project.

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(express.static('public'));
 //Get all movies
 app.get(
   '/movies',
-  passport.authenticate('jwt', { session: false }),
+  //passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
@@ -88,7 +88,7 @@ app.get(
 //get a movie by title
 app.get(
   '/movies/:title',
-  passport.authenticate('jwt', { session: false }),
+  //passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Movies.findOne({ Title: req.params.title })
       .then((movie) => {
