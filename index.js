6163f10a8a6429c664d5d021d2b52e16a3ec4493
Tanner Read <tread06@gmail.com>
2021-12-10 07:51:45 -0500

    Added localhost:420 to cors policy for angular testing
    Added localhost:420 to cors policy for angular testing

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const cors = require('cors');
 let allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:1234',
+  'http://localhost:420',
   'https://cinefacts.netlify.app',
 ];
 app.use(
